6b9437ca5e3216624c73dfbb24e0ea947c6f2b5c
Alex Davis <akonidavis@gmail.com>
2017-09-01 14:25:58 -1000

    added realm

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ export default class App extends React.Component {
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
         <Text>Can you hear me now?</Text>
+        <Text>Testing!</Text>
       </View>
     );
   }
